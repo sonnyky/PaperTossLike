6d36cff699d53411aa32743ad2db06dee439a6a2
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2015-10-25 11:40:40 +0900

    Setting up unity for git

---
+++ Assets/MyScripts/MenuButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnClick()
+    {
+        Debug.Log("Button click!");
+        Application.Quit();
+
+    }
+}
