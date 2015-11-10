d3a947e0a2700feb96029e7ecdab568d2b13cea0
Shogen, Shuhei | Lethe | GCSD <shuhei.shogen@rakuten.com>
2015-11-10 23:07:10 +0900

    [BugFix]Fix an error that was occurring by swiping when there are no balls

--- Assets/Resources/MyScripts/ShootScript.cs
+++ Assets/Resources/MyScripts/ShootScript.cs
@@ -41,56 +41,56 @@ public class ShootScript : MonoBehaviour {
 
             if (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)
             {
+				if (can_swipe == true)
+				{
+	                //Get distance vector between ball and recycle bin. Only X and Z axes matters
+	                vector_bin_to_ball.x = bin_position.x - initialBallPosition.x;
+	                vector_bin_to_ball.z = bin_position.z - initialBallPosition.z;
+	                vector_bin_to_ball.y = 0f;
 
-                //Get distance vector between ball and recycle bin. Only X and Z axes matters
-                vector_bin_to_ball.x = bin_position.x - initialBallPosition.x;
-                vector_bin_to_ball.z = bin_position.z - initialBallPosition.z;
-                vector_bin_to_ball.y = 0f;
+	                distance_abs = Mathf.Sqrt((vector_bin_to_ball.x * vector_bin_to_ball.x) + (vector_bin_to_ball.z * vector_bin_to_ball.z));
 
-                distance_abs = Mathf.Sqrt((vector_bin_to_ball.x * vector_bin_to_ball.x) + (vector_bin_to_ball.z * vector_bin_to_ball.z));
+	                end.x = t.position.x;
+	                end.y = t.position.y;
+	                end.z = 0f;
+	               
+	                direction_on_screen.x = (end.x - start.x);
+	                direction_on_screen.y = (end.y - start.y);
+	                direction_on_screen.z = (end.z - start.z);
 
-                end.x = t.position.x;
-                end.y = t.position.y;
-                end.z = 0f;
-               
-                direction_on_screen.x = (end.x - start.x);
-                direction_on_screen.y = (end.y - start.y);
-                direction_on_screen.z = (end.z - start.z);
+	                magnitude = direction_on_screen.magnitude;
 
-                magnitude = direction_on_screen.magnitude;
+	                force.x = direction_on_screen.x / -100;
+	                
+	                if(force.x > 1.2)
+	                {
+	                    force.x = 1.2f;
+	                }
+	                if (force.x < -1.2)
+	                {
+	                    force.x = -1.2f;
+	                }
 
-                force.x = direction_on_screen.x / -100;
-                
-                if(force.x > 1.2)
-                {
-                    force.x = 1.2f;
-                }
-                if (force.x < -1.2)
-                {
-                    force.x = -1.2f;
-                }
+	                //Forces in y and z axes will be made constant according to position to recycle bin
+	                force.y = distance_abs*2;
+	                force.z = distance_abs*-5;
 
-                //Forces in y and z axes will be made constant according to position to recycle bin
-                force.y = distance_abs*2;
-                force.z = distance_abs*-5;
+	                //initialize ball prefab, using the camera rotation is not a good idea here, but first I just want to make this work
+	                //clone_ball = GameObject.Instantiate(ball, initialBallPosition, this.transform.rotation) as GameObject;
+	                rb = clone_ball.GetComponent<Rigidbody>();
+	                Debug.Log(force.z);
+	                Debug.Log(force.y);
+	                // Debug.Log(direction_in_world);
+	                rb.useGravity = true;
+	                rb.AddForce(force, ForceMode.Impulse);
 
-                //initialize ball prefab, using the camera rotation is not a good idea here, but first I just want to make this work
-                //clone_ball = GameObject.Instantiate(ball, initialBallPosition, this.transform.rotation) as GameObject;
-                rb = clone_ball.GetComponent<Rigidbody>();
-                Debug.Log(force.z);
-                Debug.Log(force.y);
-                // Debug.Log(direction_in_world);
-                rb.useGravity = true;
-                rb.AddForce(force, ForceMode.Impulse);
-
-                if (can_swipe == true)
-                {
                     StartCoroutine(RespawnBall());
                     can_swipe = false;
                 }
             }
         }
     }
+
     IEnumerator RespawnBall()
     {
         print(Time.time);
